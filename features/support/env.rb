f962d9d84c41d3a0c8c7dedc91949a6e66e2222c
James Smith <jgsmith@gmail.com>
2011-05-13 13:05:25 -0400

    Reflect new dev environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 # This file makes it possible to install RubyCAS-Client as a Rails plugin.
 
 $: << File.expand_path(File.dirname(__FILE__))+'/../../lib'
-$: << File.expand_path(File.dirname(__FILE__))+'/../../../fabulator/lib'
+$: << File.expand_path(File.dirname(__FILE__))+'/../../../ruby-fabulator/lib'
 
 require 'fabulator'
 require 'fabulator/xml'
